94aa0d75ce1cf5044913983aa0bc896ba16f1832
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-08-29 19:01:27 +0200

    chore: Add optimize-css-assets-webpack-plugin and terser-webpack-plugin to minify css and javascript

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,14 +2,19 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: './src/client/index.js',
   mode: 'production',
   output: {
     libraryTarget: 'var',
-    library: 'Client'
-},
+    library: 'Client',
+  },
+  optimization: {
+    minimizer: [new TerserWebpackPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
   module: {
     rules: [
       {
