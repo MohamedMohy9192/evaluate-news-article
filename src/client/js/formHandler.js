5c5ee592c91cb39fc64f28b30b404ff7d620f4e5
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2021-08-29 18:09:36 +0200

    Add fetch request to the API

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,52 @@
+import { checkUrl } from './checkURL';
+
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+  const url = document.getElementById('article-url').value;
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+  console.log('URL', url);
+  //check url valid
+
+  if (checkUrl(url)) {
+    //Post data to the server
+    console.log('Post data to the server');
+
+    postUrl('http://localhost:8082/meaningCloudData', { url }).then((data) =>
+      updateUI(data)
+    );
+  } else {
+    alert('Not valid URL')
+  }
 }
 
-export { handleSubmit }
+const postUrl = async (url = '', data = {}) => {
+  console.log(data);
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+function updateUI(data) {
+  document.getElementById('text').innerHTML = data.text;
+  document.getElementById('agreement').innerHTML = data.agreement;
+  document.getElementById('subjectivity').innerHTML = data.subjectivity;
+  document.getElementById('confidence').innerHTML = data.confidence;
+  document.getElementById('irony').innerHTML = data.irony;
+  document.getElementById('score_tag').innerHTML = data.scoreTag;
+}
+
+export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,73 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(express.static('dist'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-console.log(__dirname)
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.static('dist'));
+
+console.log(__dirname);
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+  res.sendFile('dist/index.html')
+ // res.sendFile(path.resolve('src/client/views/index.html'));
+});
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-    console.log(`Your API key is ${process.env.API_KEY}`);
-})
+app.listen(8082, function () {
+  console.log('Example app listening on port 8081!');
+  console.log(`Your API key is ${process.env.API_KEY}`);
+});
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+  res.send(mockAPIResponse);
+});
+
+app.post('/meaningCloudData', (req, res) => {
+  const { url } = req.body;
+
+  const BASE_API_URL = 'https://api.meaningcloud.com/sentiment-2.1';
+  const MEAN_CLOUD_API_KEY = process.env.API_KEY;
+
+  const requestURL = `${BASE_API_URL}?key=${MEAN_CLOUD_API_KEY}&url=${url}&lang=en`
+  console.log(requestURL)
+
+  getMeaningCloudData(requestURL)
+  .then((data)=> {
+  
+    const requestedData = {
+      text: data.sentence_list[0].text,
+      scoreTag: data.score_tag,
+      agreement: data.agreement,
+      subjectivity: data.subjectivity,
+      confidence: data.confidence,
+      irony: data.irony
+    }
+   res.send(requestedData)
+   console.log(requestedData)
+  })
+
+});
+
+
+const fetch = require('node-fetch');
+
+const getMeaningCloudData = async (url) => {
+    const response = await fetch(url);
+    console.log(url);
+    try {
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
